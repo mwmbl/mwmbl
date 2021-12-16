e4d2a45d6c5f61a124c44e3e2845f3a4546a3d89
Daoud Clarke <daoud.clarke@gmail.com>
2021-12-16 22:26:50 +0000

    Add css

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ logging.basicConfig()
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
