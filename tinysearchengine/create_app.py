8cfb8b7a44bfbe7dda33c25467b473c918b59bea
Daoud Clarke <daoud.clarke@gmail.com>
2021-12-26 08:47:33 +0000

    Remove debug print code

--- tinysearchengine/create_app.py
+++ tinysearchengine/create_app.py
@@ -63,16 +63,13 @@ def create(tiny_index: TinyIndex):
                 last_match_char = match.span()[1]
                 seen_matches.add(value)
 
-        # num_words = len(re.findall(r'\b\w+\b', result_string))
         total_possible_match_length = sum(len(x) for x in terms)
         score = (match_length + 1./last_match_char) / (total_possible_match_length + 1)
-        # print("Score result", match_length, last_match_char, score, result.title)
         return score
 
     def order_results(terms: list[str], results: list[Document]):
         results_and_scores = [(score_result(terms, result), result) for result in results]
         ordered_results = sorted(results_and_scores, key=itemgetter(0), reverse=True)
-        # print("Ordered results", ordered_results)
         filtered_results = [result for score, result in ordered_results if score > SCORE_THRESHOLD]
         return filtered_results
 
@@ -82,21 +79,15 @@ def create(tiny_index: TinyIndex):
         results = [item.title.replace("\n", "") + ' — ' +
                    item.url.replace("\n", "") for item in ordered_results]
         if len(results) == 0:
-            # print("No results")
             return []
-        # print("Results", results)
         return [q, results]
 
-    # TODO: why does 'leek and potato soup' result not get returned for 'potato soup' query?
     def get_results(q):
         terms = [x.lower() for x in q.replace('.', ' ').split()]
-        # completed = complete_term(terms[-1])
-        # terms = terms[:-1] + [completed]
         pages = []
         seen_items = set()
         for term in terms:
             items = tiny_index.retrieve(term)
-            print("Items", items)
             if items is not None:
                 for item in items:
                     if term in item.title.lower() or term in item.extract.lower():
