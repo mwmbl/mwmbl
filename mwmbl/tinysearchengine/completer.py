fe6ace93e6efa55adfae19261a5308a3e239a903
Daoud Clarke <daoud.clarke@gmail.com>
2022-01-31 21:20:59 +0000

    Improve handling of incomplete words:  - Correctly generate regex for incomplete vs complete words  - Return more than one top word from completer  - Correctly handle no terms

--- mwmbl/tinysearchengine/completer.py
+++ mwmbl/tinysearchengine/completer.py
@@ -6,21 +6,26 @@ from pandas import DataFrame
 
 
 class Completer:
-    def __init__(self, terms: DataFrame):
+    def __init__(self, terms: DataFrame, num_matches: int = 3):
         terms_dict = terms.sort_values('term').set_index('term')['count'].to_dict()
         self.terms = list(terms_dict.keys())
         self.counts = list(terms_dict.values())
+        self.num_matches = num_matches
         print("Terms", self.terms[:100], self.counts[:100])
 
-    def complete(self, term):
+    def complete(self, term) -> list[str]:
         term_length = len(term)
-        start = bisect_left(self.terms, term, key=lambda x: x[:term_length])
-        end = bisect_right(self.terms, term, key=lambda x: x[:term_length])
+        start_index = bisect_left(self.terms, term, key=lambda x: x[:term_length])
+        end_index = bisect_right(self.terms, term, key=lambda x: x[:term_length])
 
-        matching_terms = zip(self.counts[start:end], self.terms[start:end])
-        top_count, top_term = max(matching_terms)
-        print("Top term", top_term, top_count)
-        return top_term
+        matching_terms = zip(self.counts[start_index:end_index], self.terms[start_index:end_index])
+        top_terms = sorted(matching_terms, reverse=True)[:self.num_matches]
+        print("Top terms, counts", top_terms)
+        if not top_terms:
+            return []
+
+        counts, terms = zip(*top_terms)
+        return list(terms)
 
 
 if __name__ == '__main__':
