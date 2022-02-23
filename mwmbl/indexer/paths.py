00d18c34749714eda6ce5df712433e4227903a40
Daoud Clarke <daoud.clarke@gmail.com>
2022-02-23 21:59:24 +0000

    Remove unused code

--- mwmbl/indexer/paths.py
+++ mwmbl/indexer/paths.py
@@ -6,9 +6,6 @@ HOME = os.getenv('HOME')
 DATA_DIR = Path(os.environ['HOME']) / 'data' / 'tinysearch'
 COMMON_CRAWL_TERMS_PATH = DATA_DIR / 'common-craw-terms.csv'
 
-HN_TOP_PATH = os.path.join(DATA_DIR, 'hn-top.csv')
-CRAWL_PREFIX = 'crawl_'
-CRAWL_GLOB = os.path.join(DATA_DIR, f"{CRAWL_PREFIX}*")
 TEST_INDEX_PATH = os.path.join(DATA_DIR, 'index-test.tinysearch')
 TEST_TERMS_PATH = os.path.join(DATA_DIR, 'index-terms.csv')
 WIKI_DATA_PATH = os.path.join(DATA_DIR, 'enwiki-20210301-pages-articles1.xml-p1p41242.bz2')
