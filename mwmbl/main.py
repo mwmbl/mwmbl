96da534ca561ee80dc3fc04c89921feb9daffdd0
Daoud Clarke <daoud.clarke@gmail.com>
2022-06-19 11:34:54 +0100

    Don't add CORS on the python side

--- mwmbl/main.py
+++ mwmbl/main.py
@@ -51,12 +51,12 @@ def run():
         # Initialize FastApi instance
         app = FastAPI()
 
-        # Allow CORS requests from any site
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=["*"],
-            allow_headers=["*"],
-        )
+        # # Allow CORS requests from any site
+        # app.add_middleware(
+        #     CORSMiddleware,
+        #     allow_origins=["*"],
+        #     allow_headers=["*"],
+        # )
 
         search_router = search.create_router(ranker)
         app.include_router(search_router)
