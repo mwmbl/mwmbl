c97d946fcf1fc5050b4268a558957fe57addc357
Daoud Clarke <daoud.clarke@gmail.com>
2023-02-24 21:29:42 +0000

    Go back to processing 10,000 batches at a time

--- mwmbl/indexer/process_batch.py
+++ mwmbl/indexer/process_batch.py
@@ -16,7 +16,7 @@ def run(batch_cache: BatchCache, start_status: BatchStatus, end_status: BatchSta
         index_db = IndexDatabase(db.connection)
 
         logger.info(f"Getting batches with status {start_status}")
-        batches = index_db.get_batches_by_status(start_status, 1000)
+        batches = index_db.get_batches_by_status(start_status, 10000)
         logger.info(f"Got {len(batches)} batch urls")
         if len(batches) == 0:
             return
