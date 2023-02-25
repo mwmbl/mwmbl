7d0c55c01571717ac3eace933dba50829c046b20
Daoud Clarke <daoud.clarke@gmail.com>
2023-02-25 18:18:09 +0000

    Fix broken test

--- test/test_url_queue.py
+++ test/test_url_queue.py
@@ -9,7 +9,7 @@ def test_url_queue_empties():
     new_item_queue = Queue()
     queued_batches = Queue()
 
-    url_queue = URLQueue(new_item_queue, queued_batches, min_top_domains=0)
+    url_queue = URLQueue(new_item_queue, queued_batches, min_top_domains=1)
     new_item_queue.put([FoundURL("https://google.com", "123", 10.0, URLStatus.NEW.value, datetime(2023, 1, 19))])
 
     url_queue.update()
