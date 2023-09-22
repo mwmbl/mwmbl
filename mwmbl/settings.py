bec00cdab50ccce84ac86e83b50db599d027e849
Daoud Clarke <daoud.clarke@gmail.com>
2023-09-22 23:06:04 +0100

    Exclude additional domain

--- mwmbl/settings.py
+++ mwmbl/settings.py
@@ -31,7 +31,7 @@ SCORE_FOR_DIFFERENT_DOMAIN = 1.0
 SCORE_FOR_SAME_DOMAIN = 0.01
 EXTRA_LINK_MULTIPLIER = 0.001
 UNKNOWN_DOMAIN_MULTIPLIER = 0.001
-EXCLUDED_DOMAINS = {'web.archive.org', 'forums.giantitp.com', 'www.crutchfield.com', 'plus.google.com'}
+EXCLUDED_DOMAINS = {'web.archive.org', 'forums.giantitp.com', 'www.crutchfield.com', 'plus.google.com', 'changeporn.com'}
 CORE_DOMAINS = {
     'github.com',
     'en.wikipedia.org',
