80100ebb196325cef6aabcd35d54d3a249c88dd1
Daoud Clarke <daoud.clarke@gmail.com>
2023-12-09 04:52:48 +0000

    Fix argument order

--- mwmbl/indexer/update_urls.py
+++ mwmbl/indexer/update_urls.py
@@ -35,7 +35,7 @@ def update_urls_continuously(data_path: str, new_item_queue: Queue):
 
 
 def run(batch_cache: BatchCache, new_item_queue: Queue):
-    process_batch.run(batch_cache, BatchStatus.LOCAL, BatchStatus.URLS_UPDATED, record_urls_in_database, new_item_queue, 100)
+    process_batch.run(batch_cache, BatchStatus.LOCAL, BatchStatus.URLS_UPDATED, record_urls_in_database, 100, new_item_queue)
 
 
 def record_urls_in_database(batches: Collection[HashedBatch], new_item_queue: Queue):
