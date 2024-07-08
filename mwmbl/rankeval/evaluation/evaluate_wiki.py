772909f417f9c1240c4a58457956be73b4592a15
Daoud Clarke <daoud.clarke@gmail.com>
2024-07-08 21:58:32 +0100

    Add a wikipedia-only ranker

---
+++ mwmbl/rankeval/evaluation/evaluate_wiki.py
@@ -0,0 +1,44 @@
+"""
+Evaluate ranking where we only use Wikipedia as the index.
+"""
+import urllib
+
+import requests
+from joblib import Memory
+
+from mwmbl.rankeval.evaluation.evaluate import evaluate
+from mwmbl.tinysearchengine.indexer import Document
+
+WIKI_SEARCH_API_URL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={query}&format=json"
+WIKI_URL_FORMAT = "https://en.wikipedia.org/wiki/{title}"
+
+
+memory = Memory(location="devdata/cache")
+
+
+def get_wiki_url(title: str):
+    return WIKI_URL_FORMAT.format(title=title.replace(" ", "_"))
+
+
+@memory.cache
+def fetch_results(query: str) -> list[str]:
+    safe_query = urllib.parse.quote(query, safe="")
+    url = WIKI_SEARCH_API_URL.format(query=safe_query)
+    print("Getting url", url)
+    results = requests.get(url).json()
+    print("Results", results)
+    return [get_wiki_url(result['title']) for result in results['query']['search']]
+
+
+class WikiRanker:
+    def predict(self, query):
+        return fetch_results(query)
+
+
+def run():
+    ranker = WikiRanker()
+    evaluate(ranker, fraction=0.01)
+
+
+if __name__ == "__main__":
+    run()
