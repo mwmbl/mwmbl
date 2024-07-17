bf219c02e22c459c093072cd48b8120066878fed
Daoud Clarke <daoud.clarke@gmail.com>
2024-07-17 22:22:29 +0100

    Attempt to fit a poisson

--- analyse/estimate_unique_urls.py
+++ analyse/estimate_unique_urls.py
@@ -8,7 +8,7 @@ from random import Random
 
 import numpy as np
 from scipy.optimize import curve_fit, differential_evolution
-from scipy.stats import binom
+from scipy.stats import binom, poisson
 
 from mwmbl.tinysearchengine.indexer import TinyIndex, Document
 
@@ -49,20 +49,33 @@ def estimate_unique_urls(index_path: str, num_pages_to_sample: int = 100):
             print("Binomial", x, p, s)
             return binom(num_pages_to_sample, p).pmf(x) * s
 
+        frequencies = dict(sorted(frequencies.items()))
         freq = np.array(list(frequencies.keys()))
         values = np.array(list(frequencies.values()))
 
-        def cost_function(x):
-            p, s = x
-            value = np.sum((binomial(freq, p, s) - values) ** 2)
-            return value
+        def poiss(x, m, s):
+            return poisson.pmf(x, m) * s
 
-        result = differential_evolution(cost_function, bounds=[(0.0, 1.0), (1.0, 1e100)], args=(), maxiter=100_000, strategy='rand2bin', tol=0.001)
-        print("Result", result)
+        m_fit, s_fit = curve_fit(poiss, freq, values)[0]
+        print("Estimated parameter m", m_fit, s_fit)
 
-        p, s = result.x
-        print("Predicted values", binomial(freq, p, s))
+        predictions = poiss(freq, m_fit, s_fit)
+        print("Predictions", predictions)
         print("Actual", values)
+
+
+        #
+        # def cost_function(x):
+        #     p, s = x
+        #     value = np.sum((binomial(freq, p, s) - values) ** 2)
+        #     return value
+        #
+        # result = differential_evolution(cost_function, bounds=[(0.0, 1.0), (1.0, 1e100)], args=(), maxiter=100_000, strategy='rand2bin', tol=0.001)
+        # print("Result", result)
+        #
+        # p, s = result.x
+        # print("Predicted values", binomial(freq, p, s))
+        # print("Actual", values)
 
         # num_unique_urls = len(url_counts)
         # mean_results_per_page = total_docs / num_pages_to_sample
@@ -79,7 +92,7 @@ def estimate_unique_urls(index_path: str, num_pages_to_sample: int = 100):
 
 
 if __name__ == "__main__":
-    estimate = estimate_unique_urls(str(DEV_INDEX_PATH), 100)
+    estimate = estimate_unique_urls(str(DEV_INDEX_PATH), 1000)
 
     num_urls = count_unique_urls(str(DEV_INDEX_PATH))
     print(f"Actual number of unique URLs: {num_urls}")
