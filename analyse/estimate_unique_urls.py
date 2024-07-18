d2448ae101305db4aa2c61587f2fd54e520077d1
Daoud Clarke <daoud.clarke@gmail.com>
2024-07-18 21:13:02 +0100

    Estimate counts instead of using a hyperloglog

--- analyse/estimate_unique_urls.py
+++ analyse/estimate_unique_urls.py
@@ -7,8 +7,11 @@ from pathlib import Path
 from random import Random
 
 import numpy as np
+from pydistinct.ensemble_estimators import median_estimator
+from pydistinct.stats_estimators import bootstrap_estimator, goodmans_estimator, smoothed_jackknife_estimator, \
+    horvitz_thompson_estimator
 from scipy.optimize import curve_fit, differential_evolution
-from scipy.stats import binom, poisson
+from scipy.stats import binom, poisson, betabinom
 
 from mwmbl.tinysearchengine.indexer import TinyIndex, Document
 
@@ -41,58 +44,11 @@ def estimate_unique_urls(index_path: str, num_pages_to_sample: int = 100):
             url_counts.update({doc.url for doc in page})
             total_docs += len(page)
 
-        frequencies = Counter(url_counts.values())
-        print("Frequencies", frequencies)
-
-        # Fit a binomial distribution to the data
-        def binomial(x, p, s):
-            print("Binomial", x, p, s)
-            return binom(num_pages_to_sample, p).pmf(x) * s
-
-        frequencies = dict(sorted(frequencies.items()))
-        freq = np.array(list(frequencies.keys()))
-        values = np.array(list(frequencies.values()))
-
-        def poiss(x, m, s):
-            return poisson.pmf(x, m) * s
-
-        m_fit, s_fit = curve_fit(poiss, freq, values)[0]
-        print("Estimated parameter m", m_fit, s_fit)
-
-        predictions = poiss(freq, m_fit, s_fit)
-        print("Predictions", predictions)
-        print("Actual", values)
-
-
-        #
-        # def cost_function(x):
-        #     p, s = x
-        #     value = np.sum((binomial(freq, p, s) - values) ** 2)
-        #     return value
-        #
-        # result = differential_evolution(cost_function, bounds=[(0.0, 1.0), (1.0, 1e100)], args=(), maxiter=100_000, strategy='rand2bin', tol=0.001)
-        # print("Result", result)
-        #
-        # p, s = result.x
-        # print("Predicted values", binomial(freq, p, s))
-        # print("Actual", values)
-
-        # num_unique_urls = len(url_counts)
-        # mean_results_per_page = total_docs / num_pages_to_sample
-        # initial_p = mean_results_per_page / num_unique_urls
-        # print("Initial p", initial_p)
-        # print("Initial s", num_unique_urls)
-        # p0 = [initial_p, num_unique_urls]
-        # f0 = binomial(freq, *p0)
-        # print("F0", f0)
-        #
-        # p_fit, s_fit = curve_fit(binomial, freq, values, bounds=[(0.0, 1.0), (1.0, 1e100)],
-        #                          p0=p0)[0]
-        # print("Estimated parameters p, s", p_fit, s_fit)
+    return median_estimator(attributes=dict(url_counts.items()))
 
 
 if __name__ == "__main__":
-    estimate = estimate_unique_urls(str(DEV_INDEX_PATH), 1000)
+    estimate = estimate_unique_urls(str(DEV_INDEX_PATH), 500)
 
     num_urls = count_unique_urls(str(DEV_INDEX_PATH))
     print(f"Actual number of unique URLs: {num_urls}")
