72f3a79d28709807669bfde0650a3f454703c693
Daoud Clarke <daoud.clarke@gmail.com>
2024-08-25 21:37:21 +0100

    Disable redis caching

--- mwmbl/settings_common.py
+++ mwmbl/settings_common.py
@@ -182,14 +182,6 @@ BATCH_DIR_NAME = 'batches'
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379"),
-        "KEY_PREFIX": "c_",
-    }
-}
-
 SETUP_DATABASE = True
 
 
