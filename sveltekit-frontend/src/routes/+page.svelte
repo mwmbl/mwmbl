be66ca4cc99d5cb49bc320e91258d0b230544687
xyny <git@xyny.anonaddy.me>
2024-09-08 12:42:34 +0300

    chore(frontend): add tailwind and shadcn-svelte, initial unstyled UI

--- sveltekit-frontend/src/routes/+page.svelte
+++ sveltekit-frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Input from "@/components/ui/input/input.svelte";
+</script>
+
+<h1>mwmbl</h1>
+<h2>The first <span>truly</span> open source search engine</h2>
+<p>Mwmbl (pronounced "mumble") is the first search engine that's completely independent, open source, and non-profit.</p>
+
+<Input type="search" placeholder="Search with Mwmbl..."></Input>
