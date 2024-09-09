ecf0ee77943b7e5699e9d0fd1d858f1ea8e8b7c2
xyny <git@xyny.anonaddy.me>
2024-09-09 19:45:57 +0300

    feat: add new tab toggle for results

--- sveltekit-frontend/src/routes/search/+page.svelte
+++ sveltekit-frontend/src/routes/search/+page.svelte
@@ -1,16 +1,36 @@
-<script>
+<script lang="ts">
 	import Search from '@/components/custom/Search.svelte';
 	import * as Card from '$lib/components/ui/card';
+    import { Switch } from "$lib/components/ui/switch";
+    import { Label } from "$lib/components/ui/label";
+	import { Skeleton } from '@/components/ui/skeleton';
 
 	let { data } = $props();
+
+    let openInNewTab: boolean | undefined = $state(undefined);
+	$effect(() => {
+		const value = window.localStorage.getItem("openInNewTab")
+		if (value) openInNewTab = JSON.parse(value)
+		else openInNewTab = false
+	})
+    $effect(() => {
+        window.localStorage.setItem("openInNewTab", JSON.stringify(openInNewTab))
+    })
 </script>
 
 <main class="flex flex-col items-center gap-8">
 	<Search query={data.query} />
-	<p></p>
-	<div class="flex max-w-2xl flex-col gap-4 px-4">
+    <div class="flex flex-row items-center justify-start gap-3 max-w-2xl w-full px-4">
+		{#if openInNewTab === undefined}
+			<Skeleton class="h-[24px] w-[44px] rounded-full bg-input" />
+		{:else}
+        	<Switch id="newtab-switch" class="switch-fade" bind:checked={openInNewTab} />
+		{/if}
+        <Label for="newtab-switch">Open results in new tab</Label>
+    </div>
+    <div class="flex max-w-2xl flex-col gap-4 px-4">
 		{#each data.results as result}
-			<a href={result.url} class="group">
+			<a href={result.url} class="group" target={openInNewTab ? "_blank" : "_self"}>
 				<Card.Root class="flex flex-col gap-2 p-4">
 					<div class="group-hover:underline">
 						{result.url}
@@ -39,3 +59,17 @@
 		{/each}
 	</div>
 </main>
+
+<style>
+	@keyframes switch-fade {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+	:global(.switch-fade) {
+		animation: switch-fade 0.2s ease-in-out;
+	}
+</style>
