6e693bbdaa53db119b70884b87d70b9394529556
Daoud Clarke <daoud.clarke@gmail.com>
2025-01-27 15:04:28 +0000

    Log less frequently to Sentry so we don't use up our quota

--- mwmbl/settings_common.py
+++ mwmbl/settings_common.py
@@ -195,7 +195,7 @@ if SENTRY_DSN is not None:
         dsn=SENTRY_DSN,
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for tracing.
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.1,
         # Set profiles_sample_rate to 1.0 to profile 100%
         # of sampled transactions.
         # We recommend adjusting this value in production.
