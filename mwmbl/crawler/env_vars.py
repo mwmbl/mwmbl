9a9ceaac28ba2f863f9146e98c58f289ef9bbf6b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-06-18 19:15:53 +0200

    add check for api key
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- mwmbl/crawler/env_vars.py
+++ mwmbl/crawler/env_vars.py
@@ -16,7 +16,8 @@ CRAWL_DELAY_SECONDS = float(os.environ.get("CRAWL_DELAY_SECONDS", "0.0"))
 REDIS_URL = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")
 
 # API configuration
-MWMBL_API_KEY = os.environ["MWMBL_API_KEY"]
+MWMBL_API_KEY = os.environ.get("MWMBL_API_KEY", "")
+assert MWMBL_API_KEY.strip(), "An environment variable MWMBL_API_KEY must be set to run the crawler"
 
 # Contact information configuration - required for responsible crawling
 MWMBL_CONTACT_INFO = os.environ.get("MWMBL_CONTACT_INFO", "CHANGE_ME@example.com")
